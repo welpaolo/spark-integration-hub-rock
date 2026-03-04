c1fabd8f4fda34ef4ac172e65eb781f5179a1318
Paolo Sottovia <paolo.sottovia@canonical.com>
2026-03-04 13:08:16 +0000

    Update truststore name

--- files/bin/monitor_sa.sh
+++ files/bin/monitor_sa.sh
@@ -5,4 +5,5 @@ python3 -m opt.hub.scripts.monitor_sa \
 --config=${SPARK_PROPERTIES_FILE} \
 --allowlist=${SA_ALLOWLIST} \
 --truststore=${TRUSTSTORE_PATH} \
+--truststore-secret-name=${TRUSTSTORE_SECRET_NAME} \
 --timeout=30
